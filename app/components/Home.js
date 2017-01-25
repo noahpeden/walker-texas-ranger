5fefbea0273165c2c346e099ce3580c31178e4ad
Noah Peden <noahpeden@gmail.com>
2017-01-25 13:27:18 -0700

    set up random joke and 5 jokes mapping to dom, router needs functionality

---
+++ app/components/Home.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { render } from 'react-dom';
+import '../styles';
+import Header from './Header/Header';
+import InputArea from './Main/inputArea';
+
+
+
+class Home extends React.Component{
+  constructor(){
+  super()
+  this.state = {
+    jokes: [],
+    randomJoke: '',
+    jokesNum: '',
+    }
+  }
+
+  handleChange(e){
+    this.setState({
+      jokesNum: e.target.value,
+    })
+  }
+
+  jokesCheck() {
+      let display = this.state.jokes.map((joke)=> {
+        return <li key={joke.id}>{joke.joke}</li>
+      })
+      return display;
+  }
+
+  getNewJokes(){
+    const jokeURL = `http://api.icndb.com/jokes/random/${this.state.jokesNum}/?escape=javascript`
+    fetch(jokeURL)
+          .then((response)=>{
+              return response.json()})
+          .then((data)=> {
+      this.setState({ jokes: data.value })
+    })
+  }
+
+  componentDidMount(){
+    const randomJokeURL = 'http://api.icndb.com/jokes/random/?escape=javascript'
+    fetch(randomJokeURL).then((response)=>{
+      return response.json()
+    }).then((data)=> {
+      this.setState({ randomJoke: data.value.joke })
+    })
+  }
+    render(){
+      return(
+        <div>
+        <Header />
+        <div className="random-joke">{this.state.randomJoke}</div>
+        <InputArea
+          handleChange={this.handleChange.bind(this)}           jokesNum={this.state.jokesNum}
+          getNewJokes={this.getNewJokes.bind(this)}
+        />
+        <ul>{this.jokesCheck()}</ul>
+        </div>
+      )
+    }
+  }
+
+
+
+  export default Home;
